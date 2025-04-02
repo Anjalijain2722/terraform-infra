d7bf428909f0d5e5021a9678eb3005c1be780abd
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-02 14:19:03 +0530

    Update main.tf

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -3,5 +3,5 @@ resource "aws_elasticache_cluster" "redis" {
   engine              = "redis"
   node_type           = "cache.t3.micro"
   num_cache_nodes     = 1
-  parameter_group_name = "default.redis6.x"
+  parameter_group_name = "default.redis7"
 }
