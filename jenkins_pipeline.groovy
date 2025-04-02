4be1d3eadf354d9b9d7dc8bb73e186e1c4809816
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-02 12:24:10 +0530

    Update jenkins_pipeline.groovy

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -1,11 +1,5 @@
 pipeline {
     agent any
-    parameters {
-        string(name: 'RESOURCE_TYPE', defaultValue: 'redis', description: 'Resource to create (redis, ec2, s3, etc.)')
-        string(name: 'REDIS_CLUSTER_ID', defaultValue: 'redis-cluster', description: 'Redis Cluster ID')
-        string(name: 'REDIS_NODE_TYPE', defaultValue: 'cache.t2.micro', description: 'Redis Node Type')
-        string(name: 'REDIS_NUM_NODES', defaultValue: '1', description: 'Number of Redis Nodes')
-    }
     environment {
         TF_WORKDIR = "${WORKSPACE}"
     }
@@ -17,25 +11,18 @@ pipeline {
         }
         stage('Terraform Init') {
             steps {
-                dir("${TF_WORKDIR}") {
+                dir(WORKSPACE) {
                     sh 'terraform init'
                 }
             }
         }
         stage('Terraform Apply') {
             steps {
-                dir("${TF_WORKDIR}") {
-                    script {
-                        def command = "terraform apply -auto-approve "
-                        if (params.RESOURCE_TYPE == 'redis') {
-                            command += "-var 'redis_cluster_id=${params.REDIS_CLUSTER_ID}' "
-                            command += "-var 'redis_node_type=${params.REDIS_NODE_TYPE}' "
-                            command += "-var 'redis_num_nodes=${params.REDIS_NUM_NODES}'"
-                        }
-                        sh command
-                    }
+                dir(WORKSPACE) {
+                    sh 'terraform apply -auto-approve'
                 }
             }
         }
     }
 }
+
