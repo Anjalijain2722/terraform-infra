b0b3261cc0af681b76fe2d6bcff90bd5224508db
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-02 21:26:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "redis" {
   source = "./modules/redis"
-  cluster_id       = var.selected_cluster_id
+  cluster_ids      = var.selected_cluster_id
   node_type        = var.redis_node_type
   num_cache_nodes  = var.redis_num_nodes
 
