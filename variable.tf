bac484e5631175fc02641d0ded854c2baedf4d50
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-02 15:51:28 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,15 @@
 variable "redis_cluster_id" {}
 variable "redis_node_type" {}
 variable "redis_num_nodes" {}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  default = "my-vpc"
+}
