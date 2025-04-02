1029c69e6663d68a233d4839e0d384ed83f33c40
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-02 21:36:59 +0530

    Update variable.tf

--- modules/redis/variable.tf
+++ modules/redis/variable.tf
@@ -1,13 +1,7 @@
-variable "cluster_ids" {
-  description = "List of available cluster IDs"
-  type        = list(string)
-  default     = ["redis-cluster", "redis-cluster-1", "redis-cluster-2"]
+variable "cluster_id" {
+     description = "The ID of the Redis cluster"
+     type        = string
 }
-variable "selected_cluster_id" {
-  description = "The ID of the selected Redis cluster"
-  type        = string
-}
-
 variable "node_type" {
     description = "The instance type for Redis nodes"
     type        = string
