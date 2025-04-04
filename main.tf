65f0f6b3e6177bae2613a48ac649d96a5bc1cadb
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-04 15:55:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-# VPC Module (runs only if creating vpc)
+# VPC Module (only if creating VPC)
 module "vpc" {
   source   = "./modules/vpc"
   count    = lower(var.resource_type) == "vpc" ? 1 : 0
@@ -10,9 +10,10 @@ module "vpc" {
   vpc_name = var.vpc_name
 }
 
-# Data source to get vpc_id from existing remote state
+# Data source to get VPC ID from existing remote state (only when not creating VPC)
 data "terraform_remote_state" "vpc" {
   backend = "s3"
+  count   = lower(var.resource_type) != "vpc" ? 1 : 0
   config = {
     bucket = "reddis-testing-bucket"
     key    = "terraform.tfstate" 
@@ -20,26 +21,19 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-# Redis Module
+# Redis Module (use existing VPC)
 module "redis" {
   source = "./modules/redis"
   count  = lower(var.resource_type) == "redis" || lower(var.resource_type) == "ElastiCache-Redis" ? 1 : 0
   cluster_id       = var.redis_cluster_id
   node_type        = var.redis_node_type
   num_cache_nodes  = var.redis_num_nodes
-  vpc_id           = data.terraform_remote_state.vpc.outputs.vpc_id
-}
-
-# Error handling: If Redis is selected but no VPC output found
-resource "null_resource" "fail_if_no_vpc" {
-  count = lower(var.resource_type) == "ElastiCache-Redis" && data.terraform_remote_state.vpc.outputs.vpc_id == "" ? 1 : 0
-
-  provisioner "local-exec" {
-    command = "echo 'ERROR: No VPC found. Please create the VPC first using resource_type = vpc' && exit 1"
-  }
+  vpc_id           = lower(var.resource_type) == "vpc" ? module.vpc[0].vpc_id : data.terraform_remote_state.vpc[0].outputs.vpc_id
 }
 
 # Output only when VPC is created
 output "vpc_id" {
-  value = var.resource_type == "vpc" || var.resource_type == "ElastiCache-Redis" ? module.vpc[0].vpc_id : null
+  value = lower(var.resource_type) == "vpc" ? module.vpc[0].vpc_id :
+          lower(var.resource_type) == "Elasticache-Redis" ? data.terraform_remote_state.vpc[0].outputs.vpc_id : null
 }
+
