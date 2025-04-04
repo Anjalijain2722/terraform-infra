5422a82994e4cddb78ff6de71bd7321cf00b3db4
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-04 17:28:41 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,32 +1,9 @@
 variable "resource_type" {
-  type    = string
+  type = string
   description = "Resource to create: vpc or redis"
-}
 
-variable "region" {
-  type    = string
-  default = "ap-south-1"
-}
-
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "vpc_name" {
-  type    = string
-  default = "my-vpc"
-}
-
-
-variable "redis_cluster_id" {
-  type = string
-}
-
-variable "redis_node_type" {
-  type = string
-}
-
-variable "redis_num_nodes" {
-  type = number
+  validation {
+    condition     = contains(["vpc", "redis"], lower(var.resource_type))
+    error_message = "resource_type must be either 'vpc' or 'redis'"
+  }
 }
