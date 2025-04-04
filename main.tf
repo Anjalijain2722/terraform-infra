447fa653e498e08be349bba958ae09b8eae46f59
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-04 12:39:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,24 @@
+variable "resource_type" {
+  description = "Choose 'vpc' or 'redis'"
+  type        = string
+}
+
+module "vpc" {
+  source   = "./modules/vpc"
+  count    = var.resource_type == "vpc" || var.resource_type == "redis" ? 1 : 0
+  region   = var.region
+  vpc_cidr = var.vpc_cidr
+  vpc_name = var.vpc_name
+}
+
 module "redis" {
   source = "./modules/redis"
+  count  = var.resource_type == "redis" ? 1 : 0
+
   cluster_id       = var.redis_cluster_id
   node_type        = var.redis_node_type
   num_cache_nodes  = var.redis_num_nodes
 
-}
 
-module "vpc" {
-  source    = "./modules/vpc"
-  region    = var.region
-  vpc_cidr  = var.vpc_cidr
-  vpc_name  = var.vpc_name
+  vpc_id           = module.vpc[0].vpc_id 
 }
