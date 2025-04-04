c5af981634d75196c7f588340589197fb05e0ed8
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-04 17:47:42 +0530

    Update main.tf

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -1,22 +1,27 @@
-resource "aws_elasticache_subnet_group" "this" {
+variable "cluster_id" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "subnet_ids" {
+  type = list(string)
+}
+
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   name       = "${var.cluster_id}-subnet-group"
   subnet_ids = var.subnet_ids
 }
 
-resource "aws_security_group" "redis_sg" {
-  name        = "${var.cluster_id}-sg"
-  description = "Security group for Redis"
-  vpc_id      = var.vpc_id
-}
-
 resource "aws_elasticache_cluster" "redis" {
-  count                = var.vpc_id != "" ? 1 : 0
   cluster_id           = var.cluster_id
   engine               = "redis"
   node_type            = var.node_type
   num_cache_nodes      = var.num_cache_nodes
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
   parameter_group_name = "default.redis7"
-  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group[0].name
+  port                 = 6379
 
   depends_on = [aws_elasticache_subnet_group.redis_subnet_group]
+
+  tags = {
+    Name = var.cluster_id
+  }
 }
