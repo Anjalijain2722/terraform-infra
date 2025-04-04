d788fae68130fd4ed5faa260087d88abfd0a8ca2
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-04 22:21:45 +0530

    Update variable.tf

--- modules/redis/variable.tf
+++ modules/redis/variable.tf
@@ -1,23 +1,9 @@
-variable "cluster_id" {
-  type        = string
-  description = "The ID of the Redis cluster"
-}
-
-variable "node_type" {
-  type        = string
-  description = "The node type for the Redis cluster"
-}
-
-variable "num_cache_nodes" {
-  type        = number
-  description = "The number of Redis nodes"
-}
 variable "vpc_id" {
+  description = "VPC ID where Redis will be deployed"
   type        = string
-  description = "VPC ID for Redis deployment"
 }
 
 variable "subnet_ids" {
+  description = "Subnets for Redis cluster"
   type        = list(string)
-  description = "List of subnet IDs for Redis deployment"
 }
