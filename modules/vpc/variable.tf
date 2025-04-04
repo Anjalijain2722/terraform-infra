e571f2cdcfbec77ba03f2f63c004e247c084337c
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-04 22:20:37 +0530

    Update variable.tf

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -1,9 +1,5 @@
-variable "vpc_cidr" {
-  type        = string
+variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
+  type        = string
 }
 
-variable "vpc_name" {
-  type        = string
-  description = "Name of the VPC"
-}
