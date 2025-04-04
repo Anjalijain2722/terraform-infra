708b3b0316e40fc12b0c3f0a7bb908662d3f1025
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-04 15:01:27 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,40 +1,32 @@
 variable "resource_type" {
-  description = "Choose 'vpc' or 'redis'"
-  type        = string
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  type    = string
+  description = "Resource to create: vpc or redis"
 }
 
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "vpc_name" {
-  description = "Name for the VPC"
-  type        = string
-  default     = "my-vpc"
+  type    = string
+  default = "my-vpc"
 }
 
+
 variable "redis_cluster_id" {
-  description = "Redis cluster ID"
-  type        = string
-  default     = "redis-cluster"
+  type = string
 }
 
 variable "redis_node_type" {
-  description = "Node type"
-  type        = string
-  default     = "cache.t3.micro"
+  type = string
 }
 
 variable "redis_num_nodes" {
-  description = "Number of Redis nodes"
-  type        = number
-  default     = 1
+  type = number
 }
