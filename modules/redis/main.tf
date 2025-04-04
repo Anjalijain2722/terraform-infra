60d14745243ed40ebd96a9312fcd051da70499d6
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-04 15:57:25 +0530

    Update main.tf

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -10,4 +10,5 @@ resource "aws_elasticache_cluster" "redis" {
   num_cache_nodes     = 1
   parameter_group_name = "default.redis7"
   subnet_group_name   = aws_elasticache_subnet_group.redis_subnet_group.name
+  vpc_security_group_ids = [var.security_group_id]
 }
