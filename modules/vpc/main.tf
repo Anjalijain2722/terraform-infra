e5a56c91b38c616cf8881c2283d6797da82acc53
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 17:48:39 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,9 +1,11 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-   lifecycle {
-    prevent_destroy = true
-  }
+
   tags = {
     Name = var.vpc_name
+  }
+  
+  lifecycle {
+    prevent_destroy = true
   }
 }
