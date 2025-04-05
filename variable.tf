1238dea27676970fc96e7a48a682531486b02950
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 16:03:33 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,20 +1,14 @@
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
+  default = "ap-south-1"
 }
 
-variable "resource_type" {
-  description = "Choose which resource to create (vpc or redis)"
-  type        = string
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
 }
 
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+variable "vpc_name" {
+  default = "my-vpc"
 }
-
 
 
 
