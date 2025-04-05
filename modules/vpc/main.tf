ab070d9eaa80d1a5560aa35821592aef5c82f863
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 12:47:52 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,8 +1,16 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = "my-vpc"
-  }
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet("10.0.0.0/16", 4, count.index)
+  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
+}
+
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
 }
 
 
