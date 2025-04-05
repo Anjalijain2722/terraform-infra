aca0c6cf4ba12c3f6a0a80751b8de864f74708a1
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 15:12:44 +0530

    Update main.tf

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -16,31 +16,6 @@ resource "aws_elasticache_subnet_group" "redis" {
   subnet_ids = var.subnet_ids
 }
 
-resource "aws_security_group" "redis_sg" {
-  name        = "redis-sg"
-  description = "Allow Redis traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 6379
-    to_port     = 6379
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Restrict this in real setup
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "redis-sg"
-  }
-}
-
-
 resource "aws_elasticache_cluster" "ElastiCache-Redis" {
   cluster_id           = "redis-cluster-demo"
   engine               = "redis"
