230bd30cab5c9ed610bc734e8c80c501fa7f0405
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 15:22:23 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,18 +1,11 @@
 variable "resource_type" {
+  description = "Type of resource to create"
   type        = string
-  description = "Specify what to deploy: VPC or ElastiCache-Redis"
-}
-
-variable "aws_region" {
-  type        = string
-  description = "AWS region"
-  default     = "ap-south-1"
 }
 
 variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
   type        = string
-  description = "CIDR block for VPC"
-  default     = "10.0.0.0/16"
 }
 
 
