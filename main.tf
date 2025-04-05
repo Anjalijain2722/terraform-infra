eb3741b7580667b4c5892f2a4f2e719a480f68db
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 13:48:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
-required_providers {
+terraform {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "redis-testing-bucket-new"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
   }
 }
 
