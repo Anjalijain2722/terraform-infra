d47150fc70847e371e30aa4216c069ceae7164ad
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 12:46:11 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,16 +1,5 @@
-variable "resource" {
-  description = "Select which resource to deploy: 'vpc' or 'redis'"
+variable "resource_type" {
+  description = "Resource to create: vpc or redis"
   type        = string
 }
 
-variable "vpc_cidr_block" {
-  type        = string
-  description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet_ids" {
-  type        = list(string)
-  description = "List of subnet IDs for Redis"
-  default     = []
-}
