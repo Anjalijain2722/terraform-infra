cb2011f7c150d4669a5ecdb55041b4641438e6dc
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 16:29:17 +0530

    Update variable.tf

--- modules/redis/variable.tf
+++ modules/redis/variable.tf
@@ -1,8 +1,18 @@
-variable "cluster_id" {}
-variable "node_type" {}
-variable "subnet_ids" {
-  type = list(string)
+variable "cluster_id" {
+     description = "The ID of the Redis cluster"
+     type        = string
 }
+variable "node_type" {
+    description = "The instance type for Redis nodes"
+    type        = string
+    default     = "cache.t3.micro"
+}
+variable "num_cache_nodes" {
+    description = "Number of shards in redis cluster node"
+    type = number
+    default = 2
+}
+
 
 
 
