3b73ed3b4ec2495e91f3f340538e33193d95df96
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 13:42:12 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,14 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
 variable "resource_type" {
-  description = "Set to 'vpc' or 'ElastiCache-Redis'"
+  description = "Type of resource to provision"
   type        = string
+}
+
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
 }
 
 
