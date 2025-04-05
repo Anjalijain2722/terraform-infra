30b6194ec0908bc56465f35c6747b874eb540d86
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 15:03:51 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,18 +1,27 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr_block
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "demo-vpc"
+  }
 }
 
 resource "aws_subnet" "public" {
   count                   = 2
-  cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index)
   vpc_id                  = aws_vpc.main.id
-  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
 }
 
-resource "aws_security_group" "vpc_sg" {
-  vpc_id = aws_vpc.main.id
-}
 
 
 
