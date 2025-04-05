b435c6da0d2b85d72fb4f102ef195635abebb15b
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 15:45:29 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,13 +1,10 @@
-output "vpc_id" {
-  value = aws_vpc.this.id
+resource "aws_subnet" "this" {
+  count             = 2
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = cidrsubnet(var.vpc_cidr_block, 8, count.index)
+  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
 }
 
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr_block
 }
-
-
-output "subnet_ids" {
-  value = aws_subnet.this[*].id
-}
-
