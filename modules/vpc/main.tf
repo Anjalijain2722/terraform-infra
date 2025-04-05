df65b31360a21d850a267d5fef9b8e2105f8bdae
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 15:41:32 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,6 +2,11 @@ output "vpc_id" {
   value = aws_vpc.this.id
 }
 
+variable "vpc_cidr_block" {
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
 output "subnet_ids" {
   value = aws_subnet.this[*].id
 }
