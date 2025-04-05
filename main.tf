2cf9dfe85a5cf0da48f9b31923466e3b83382322
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 13:41:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,45 @@
+terraform {
+  backend "s3" {
+    bucket = "redis-testing-bucket-new"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 module "vpc" {
-  count  = var.resource_type == "vpc" || var.resource_type == "ElastiCache-Redis" ? 1 : 0
+  count  = var.resource_type == "VPC" ? 1 : 0
   source = "./modules/vpc"
 
-  # add your VPC inputs here, if needed
+  vpc_cidr_block = var.vpc_cidr_block
+}
+
+data "terraform_remote_state" "vpc" {
+  count   = var.resource_type == "ElastiCache-Redis" ? 1 : 0
+  backend = "s3"
+
+  config = {
+    bucket = "redis-testing-bucket-new"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
 
 module "redis" {
   count = var.resource_type == "ElastiCache-Redis" ? 1 : 0
 
-  source              = "./modules/redis"
-  vpc_id              = module.vpc[0].vpc_id
-  subnet_ids          = module.vpc[0].subnet_ids
-  security_group_ids  = module.vpc[0].security_group_ids
-  resource_type       = var.resource_type
+  source             = "./modules/redis"
+  vpc_id             = data.terraform_remote_state.vpc[0].outputs.vpc_id
+  subnet_ids         = data.terraform_remote_state.vpc[0].outputs.subnet_ids
+  security_group_ids = data.terraform_remote_state.vpc[0].outputs.security_group_ids
 }
-
