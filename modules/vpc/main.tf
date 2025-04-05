7feb9b55ab694bccde66d595578dc4ff005759c4
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 15:38:05 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,50 +1,10 @@
-resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr_block
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "demo-vpc"
-  }
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_cidr_block, 4, count.index)
-  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
-
-  tags = {
-    Name = "public-subnet-${count.index}"
-  }
+resource "aws_subnet" "this" {
+  count                   = 2
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
 }
-
-resource "aws_security_group" "redis_sg" {
-  name        = "redis-sg"
-  vpc_id      = aws_vpc.main.id
-  description = "Allow Redis traffic"
-
-  ingress {
-    from_port   = 6379
-    to_port     = 6379
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "redis-sg"
-  }
-}
-
-
-
-
-
-
