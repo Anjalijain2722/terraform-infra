6e62182d5c4e56c658f4ef1bcd54c28ef90cf491
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 16:30:33 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,18 +1,11 @@
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+  cidr_block = var.vpc_cidr
+
   tags = {
     Name = var.vpc_name
   }
 }
-
-resource "aws_subnet" "public" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.subnet_cidrs[count.index]
-  availability_zone = var.azs[count.index]
-
-  tags = {
-    Name = "public-subnet-${count.index + 1}"
-  }
-}
-
