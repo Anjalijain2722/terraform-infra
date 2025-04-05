213297c6af8e37f2c2a47442510c47ca36010cf9
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 12:49:48 +0530

    Update variable.tf

--- modules/redis/variable.tf
+++ modules/redis/variable.tf
@@ -1,9 +1,15 @@
 variable "vpc_id" {
-  description = "VPC ID where Redis will be deployed"
+  description = "VPC ID for Redis"
   type        = string
 }
 
 variable "subnet_ids" {
-  description = "Subnets for Redis cluster"
+  description = "Subnet IDs for Redis"
   type        = list(string)
 }
+
+variable "security_group_ids" {
+  description = "Security Group IDs for Redis"
+  type        = list(string)
+}
+
