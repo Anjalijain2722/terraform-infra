c5bc3be691682b9ba4edd7b00ae459f656fefb8e
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 13:29:46 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,15 +2,31 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "main" {
-  count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 4, count.index)
+resource "aws_subnet" "subnet" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index}.0/24"
   availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
 }
 
-resource "aws_security_group" "main" {
-  vpc_id = aws_vpc.main.id
+resource "aws_security_group" "sg" {
+  name        = "redis-sg"
+  vpc_id      = aws_vpc.main.id
+  description = "Allow redis"
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
