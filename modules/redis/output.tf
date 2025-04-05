632a91db279dddaac4b663223eda0ec609049c0c
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 13:05:06 +0530

    Update output.tf

--- modules/redis/output.tf
+++ modules/redis/output.tf
@@ -1,6 +1,6 @@
 output "redis_endpoint" {
-  value       = length(aws_elasticache_cluster.redis) > 0 ? aws_elasticache_cluster.redis[0].cache_nodes[0].address : null
-  description = "Endpoint of the Redis cluster"
+  value = length(aws_elasticache_cluster.ElastiCache_redis) > 0 ? aws_elasticache_cluster.ElastiCache_redis[0].cache_nodes[0].address : null
+  description = "Redis Cluster Endpoint"
 }
 
 
