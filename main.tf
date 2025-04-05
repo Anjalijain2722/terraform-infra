4e0a56b5ae0c3d8731edd75dae6d5815c282e03c
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 15:35:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,35 @@
-provider "aws" {
-  region = "ap-south-1"
+locals {
+  is_vpc  = var.resource_type == "vpc"
+  is_redis = var.resource_type == "ElastiCache-Redis"
 }
 
 module "vpc" {
-  source          = "./modules/vpc"
-  count           = var.resource_type == "vpc" ? 1 : 0
-  vpc_cidr_block  = var.vpc_cidr_block
+  source      = "./modules/vpc"
+  count       = local.is_vpc ? 1 : 0
+  region      = var.region
 }
 
 data "terraform_remote_state" "vpc" {
+  count   = local.is_redis ? 1 : 0
   backend = "s3"
   config = {
     bucket = "redis-testing-bucket-new"
     key    = "terraform.tfstate"
-    region = "ap-south-1"
+    region = var.region
+  }
+}
+
+resource "null_resource" "vpc_check" {
+  count = local.is_redis && (length(data.terraform_remote_state.vpc[0].outputs) == 0 ? 1 : 0)
+  provisioner "local-exec" {
+    command = "echo 'VPC not found. Cannot create Redis without VPC.' && exit 1"
   }
 }
 
 module "redis" {
-  source             = "./modules/redis"
-  count              = var.resource_type == "ElastiCache-Redis" ? 1 : 0
-  vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
-  subnet_ids         = data.terraform_remote_state.vpc.outputs.public_subnet_ids
-  security_group_ids = [data.terraform_remote_state.vpc.outputs.redis_sg_id]
+  source       = "./modules/redis"
+  count        = local.is_redis ? 1 : 0
+  region       = var.region
+  vpc_id       = data.terraform_remote_state.vpc[0].outputs.vpc_id
+  subnet_ids   = data.terraform_remote_state.vpc[0].outputs.subnet_ids
 }
