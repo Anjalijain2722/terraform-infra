461b8043a89e5413d0a6f600b86714164e84f574
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 15:25:07 +0530

    Update main.tf

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -1,17 +1,4 @@
-locals {
-  vpc_provided = var.vpc_id != "" && length(var.subnet_ids) > 0
-}
-
-# Fail if VPC details not provided
-resource "null_resource" "check_vpc" {
-  count = local.vpc_provided ? 0 : 1
-
-  provisioner "local-exec" {
-    command = "echo 'ERROR: VPC or subnet_ids missing. Redis cannot be created.' && exit 1"
-  }
-}
-
-resource "aws_elasticache_subnet_group" "redis" {
+resource "aws_elasticache_subnet_group" "ElastiCache-Redis" {
   name       = "redis-subnet-group"
   subnet_ids = var.subnet_ids
 }
@@ -24,8 +11,7 @@ resource "aws_elasticache_cluster" "ElastiCache-Redis" {
   parameter_group_name = "default.redis6.x"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.redis.name
-  security_group_ids = var.security_group_ids
-
+  security_group_ids   = var.security_group_ids
 
   depends_on = [aws_elasticache_subnet_group.redis]
 }
