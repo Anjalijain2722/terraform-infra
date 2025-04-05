3ca8477a0c6c0343fcee3e8f7e39bd364b048fe9
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 12:45:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "redis-testing-bucket-new"
-    key    = "global/terraform.tfstate"
-    region = "ap-south-1"
+    bucket         = "redis-testing-bucket-new"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
   }
 }
 
