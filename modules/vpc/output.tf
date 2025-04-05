dcee11bf36d97982edfb9e35316714d77d3d96d7
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 12:48:23 +0530

    Update output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+output "subnet_ids" {
+  value = aws_subnet.main[*].id
+}
+
+output "security_group_ids" {
+  value = [aws_security_group.main.id]
+}
