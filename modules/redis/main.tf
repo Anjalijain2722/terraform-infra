a0c6836bf36b723f33b41159faac0a4a4a2986d7
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 13:44:13 +0530

    Update main.tf

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -1,27 +1,14 @@
-resource "aws_elasticache_subnet_group" "redis_subnet" {
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   name       = "redis-subnet-group"
   subnet_ids = var.subnet_ids
 }
 
-resource "aws_elasticache_cluster" "ElastiCache_Redis" {
+resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "redis-cluster"
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis7"
-  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet.name
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
   security_group_ids   = var.security_group_ids
+  parameter_group_name = "default.redis6.x"
 }
-
-locals {
-  vpc_valid = var.vpc_id != ""
-}
-
-resource "null_resource" "check_vpc" {
-  count = local.vpc_valid ? 0 : 1
-
-  provisioner "local-exec" {
-    command = "echo 'ERROR: VPC is required for Redis. Please create a VPC first.' && exit 1"
-  }
-}
-
