c5aa2a92e859c0546d07bc2eb30004c844267afb
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 13:58:45 +0530

    Update main.tf

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -1,14 +1,20 @@
-resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+resource "aws_elasticache_subnet_group" "redis_subnet" {
   name       = "redis-subnet-group"
   subnet_ids = var.subnet_ids
 }
 
-resource "aws_elasticache_cluster" "ElastiCache-Redis" {
+resource "aws_elasticache_cluster" "ElastiCache_Redis" {
   cluster_id           = "redis-cluster"
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
-  security_group_ids   = var.security_group_ids
   parameter_group_name = "default.redis6.x"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet.name
+  security_group_ids   = var.security_group_ids
+
+  tags = {
+    Name = "RedisCluster"
+  }
 }
+
