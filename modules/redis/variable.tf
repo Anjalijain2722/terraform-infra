f7cd411ffaca522da9c60e606055511716314d73
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 13:31:53 +0530

    Update variable.tf

--- modules/redis/variable.tf
+++ modules/redis/variable.tf
@@ -1,20 +1,16 @@
-variable "resource_type" {
-  description = "Type of resource to create, like ElasticCache-Redis or vpc"
-  type        = string
-}
-
 variable "vpc_id" {
-  description = "VPC ID for Redis"
-  type        = string
+  type = string
 }
 
 variable "subnet_ids" {
-  description = "Subnet IDs for Redis"
-  type        = list(string)
+  type = list(string)
 }
 
 variable "security_group_ids" {
-  description = "Security Group IDs for Redis"
-  type        = list(string)
+  type = list(string)
+}
+
+variable "resource_type" {
+  type = string
 }
 
