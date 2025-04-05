c0000e6a635d3057788802960591f5d877f471cf
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 13:59:04 +0530

    Update output.tf

--- modules/redis/output.tf
+++ modules/redis/output.tf
@@ -2,6 +2,10 @@ output "redis_endpoint" {
   value = aws_elasticache_cluster.ElastiCache_Redis.cache_nodes[0].address
 }
 
+output "redis_port" {
+  value = aws_elasticache_cluster.ElastiCache_Redis.port
+}
+
 
 
 
