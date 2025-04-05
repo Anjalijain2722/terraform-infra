6a5fd2cc7e2ff0a6933d7854a74b773a4cc574a2
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 20:46:24 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,13 +1,12 @@
-# outputs.tf (in the root directory)
-
 output "vpc_vpc_id" {
-  value = module.vpc.vpc_id
+  value = module.vpc[0].vpc_id
 }
 
 output "vpc_subnet_ids" {
-  value = module.vpc.subnet_ids
+  value = module.vpc[0].subnet_ids
 }
 
 output "vpc_redis_sg_id" {
-  value = module.vpc.redis_sg_id
+  value = module.vpc[0].redis_sg_id
 }
+
