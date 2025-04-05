32b1daf4beb8d2f1a7c387d380546abe17c95a43
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-05 13:12:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,11 @@ module "vpc" {
 }
 
 module "redis" {
-  count = var.resource_type == "ElastiCache-Redis" ? 1 : 0
-
+  count               = var.resource_type == "ElastiCache-Redis" ? 1 : 0
   source              = "./modules/redis"
-  vpc_id              = module.vpc[0].vpc_id
-  subnet_ids          = module.vpc[0].subnet_ids
-  security_group_ids  = module.vpc[0].security_group_ids
+  vpc_id              = module.vpc.vpc_id
+  subnet_ids          = module.vpc.subnet_ids
+  security_group_ids  = module.vpc.security_group_ids
+  resource_type       = var.resource_type
 }
+
