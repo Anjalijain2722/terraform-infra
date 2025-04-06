2898a53ba2d6f8ad0429bba8456b6dc99dbf4498
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-06 16:34:04 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,7 @@
 # modules/vpc/main.tf
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  prevent_destroy = true
 }
 
 resource "aws_subnet" "public" {
@@ -8,12 +9,15 @@ resource "aws_subnet" "public" {
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   vpc_id            = aws_vpc.main.id
   availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
+  prevent_destroy = true
 }
 
 resource "aws_security_group" "redis_sg" {
   name        = "redis-sg"
   description = "Security group for Redis"
   vpc_id      = aws_vpc.main.id
+
+  prevent_destroy = true
 
   ingress {
     from_port   = 6379
