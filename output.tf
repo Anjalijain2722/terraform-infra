8008cccebf19bcc26446ad0188993ed6c5c028c7
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-06 13:44:57 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,12 +1,11 @@
 output "vpc_vpc_id" {
-  value = module.vpc[0].vpc_id
+  value = length(module.vpc) > 0 ? module.vpc[0].vpc_id : null
 }
 
 output "vpc_subnet_ids" {
-  value = module.vpc[0].subnet_ids
+  value = length(module.vpc) > 0 ? module.vpc[0].subnet_ids : []
 }
 
 output "vpc_redis_sg_id" {
-  value = module.vpc[0].redis_sg_id
+  value = length(module.vpc) > 0 ? module.vpc[0].redis_sg_id : null
 }
-
