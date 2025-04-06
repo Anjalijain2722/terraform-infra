2cdd02d8dfba0e17c4ec4fea3927b288bd0b9b88
Anjali Jain <101129749+Anjalijain2722@users.noreply.github.com>
2025-04-06 17:40:22 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,3 @@
-variable "create_vpc" {
-  type    = bool
-  default = false
-}
-
-variable "create_redis" {
-  type    = bool
-  default = false
-}
 variable "resource_type" {
   description = "Resource to create - either vpc or redis"
   type        = string
